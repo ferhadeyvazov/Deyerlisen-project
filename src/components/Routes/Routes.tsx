8b6b1fbea7094232c1f1749bc9aa80e59e569516
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2024-08-12 19:26:27 +0400

    Ant Design was downloaded successfully

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import RenderIf from '@utils/RenderIf';
 import PrivateRoutes from './PrivateRoutes';
 import PublicRoutes from './PublicRoutes';
+import Loading from '@ui/Loading/Loading';
 
 // const fakeAuth = import.meta.env.VITE_FAKE_AUTH;
 const FAKE_AUTH = false;
@@ -9,12 +10,14 @@ const FAKE_AUTH = false;
 const Routes: React.FC = () => {
     return (
         <>
-            <RenderIf condition={FAKE_AUTH}>
-                <PrivateRoutes/>
-            </RenderIf>
-            <RenderIf condition={!FAKE_AUTH}>
-                <PublicRoutes />
-            </RenderIf>
+            <React.Suspense fallback={<Loading />}>
+                <RenderIf condition={FAKE_AUTH}>
+                    <PrivateRoutes />
+                </RenderIf>
+                <RenderIf condition={!FAKE_AUTH}>
+                    <PublicRoutes />
+                </RenderIf>
+            </React.Suspense>
         </>
 
     )
